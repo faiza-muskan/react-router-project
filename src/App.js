d39be2454d5ec5af46bf2fdf0dd23abcb1d862d2
Faiza Muskan <108839239+faiza-muskan@users.noreply.github.com>
2023-01-18 20:49:34 +0000

    added children path

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import { RouterProvider } from "react-router-dom";
 
-import Home from "./components/Home";
-import Product from "./components/Product";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import Roots from "./pages/Roots";
+import Error from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/product", element: <Product /> },
+  {
+    path: "/",
+    element: <Roots />,
+    errorElement: <Error />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/product", element: <Product /> },
+    ],
+  },
 ]);
 
 const App = () => {

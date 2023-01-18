17d6a746204bdb15ecdebaef903e0950959fc947
Faiza Muskan <108839239+faiza-muskan@users.noreply.github.com>
2023-01-18 16:15:59 +0000

    added paths to the pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-function App() {
-  return <div></div>;
-}
+import { createBrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+
+import Home from "./components/Home";
+import Product from "./components/Product";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/product", element: <Product /> },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
